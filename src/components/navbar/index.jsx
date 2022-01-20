38afbfdeaf6cfbbb2aa805ae79abc41b6f90b545
Amandine Arias <amandine.arias@gmail.com>
2022-01-20 17:55:15 +0100

    feat (Navbar) : avatar + icons + dynamique option menu + styles

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,19 +1,64 @@
-import logo from '../../assets/logo_netflix.svg'
+import { useState } from 'react';
+import logo from '../../assets/logo_netflix.svg';
+import {
+  ArrowDropDown,
+  ArrowDropUp,
+  Logout,
+  Notifications,
+  PowerSettingsNew,
+  Search,
+  Tune,
+} from '@mui/icons-material';
+import avatar from '../../assets/avatar_amandine.png';
 import './navbar.css';
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = (e) => {
+    e.preventDefault();
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <div className="navbar-left">
-          <img src={logo} alt="Netflix Logo" className='navbar-left--logo' />
-          <span>Homepage</span>
-          <span>Series</span>
-          <span>Movies</span>
-          <span>New & Popular</span>
-          <span>My List</span>
+          <img src={logo} alt="Netflix Logo" className="navbar-left--logo" />
+          <span className="navbar-link">Homepage</span>
+          <span className="navbar-link">Series</span>
+          <span className="navbar-link">Movies</span>
+          <span className="navbar-link">Popular</span>
+          <span className="navbar-link">My List</span>
         </div>
-        <div className="navbar-right"></div>
+        <div className="navbar-right">
+          <Search />
+          <span className="navbar-link">KID</span>
+          <Notifications />
+          <img
+            src={avatar}
+            alt="avatar Amandine"
+            className="navbar-right--avatar"
+          />
+          <span className="navbar-right--profile">
+            {isOpen ? (
+              <>
+                <ArrowDropUp onClick={handleClick} />
+                <div className="navbar-right--options">
+                  <button>
+                    <PowerSettingsNew />
+                  </button>
+                  <button>
+                    <Tune />
+                  </button>
+                  {/* <button>Settings</button>
+                <button>Logout</button> */}
+                </div>
+              </>
+            ) : (
+              <ArrowDropDown onClick={handleClick} />
+            )}
+          </span>
+        </div>
       </div>
     </nav>
   );
