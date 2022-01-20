588b47cbda7aab1cc8adabb83a3487006214f818
Amandine Arias <amandine.arias@gmail.com>
2022-01-20 15:25:11 +0100

    feat (Component) : Navbar

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,19 @@
+import './navbar.scss';
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-left">
+          <img src="" alt="" />
+          <span>Homepage</span>
+          <span>Series</span>
+          <span>Movies</span>
+          <span>New & Popular</span>
+          <span>My List</span>
+        </div>
+        <div className="navbar-right"></div>
+      </div>
+    </nav>
+  );
+}
