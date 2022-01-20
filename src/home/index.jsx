e02acdff7cb85ccc9a3107ce4a20633d894a0420
Amandine Arias <amandine.arias@gmail.com>
2022-01-20 17:52:44 +0100

    refactor (style) : classname

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -2,5 +2,5 @@ import Navbar from '../components/navbar'
 import "./home.css"
 
 export default function Home() {
-  return <div className='home' > <Navbar /> </div>;
+  return <main className='landing-page' > <Navbar /> </main>;
 }
