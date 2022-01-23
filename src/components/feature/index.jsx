7bac1e2e1dcd2b612828d4cd604bd924bf85ba87
Amandine Arias <amandine.arias@gmail.com>
2022-01-23 19:50:55 +0100

    refactor (Feature) : classnames

--- src/components/feature/index.jsx
+++ src/components/feature/index.jsx
@@ -3,7 +3,7 @@ import { InfoRounded, PlayCircleFilledRounded } from '@mui/icons-material';
 import Select from 'react-select';
 import { options } from './options';
 
-import poster from '../../assets/movie_matrix_reloaded_1.jpg';
+import poster from '../../assets/serie_true-detective_1.jpg';
 
 import './feature.css';
 
@@ -11,23 +11,23 @@ export default function Feature({ type }) {
   return (
     <section className="feature">
       {type && (
-        <div className="feature-category">
+        <div className="feature_category">
           <span>{type === 'movie' ? 'Movies' : 'Series'} </span>
           <Select
             name="genre"
             id="genre"
             options={options}
             defaultValue={options[0]}
-            className='react-select-container'
-            classNamePrefix="react-select"
+            className='react_select-container'
+            classNamePrefix="react_select"
             theme={(theme) => ({
               ...theme,
               borderRadius: 5,
               colors: {
                 ...theme.colors,
                 primary25: '#d81f26',
-                primary: 'black',
-                backgroundColor: 'black'
+                primary: "#141414",
+                backgroundColor: '#141414'
               },
             })}
           />
@@ -35,20 +35,20 @@ export default function Feature({ type }) {
       )}
 
       <img src={poster} alt="" />
-      <article className="feature-content">
-        <h1>Matrix</h1>
-        <p className="feature-description">
+      <article className="feature_content">
+        <h1>True detective</h1>
+        <p className="feature_description">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam neque
           laborum suscipit saepe? Voluptatibus atque necessitatibus, autem sequi
           pariatur dignissimos magnam incidunt assumenda, optio explicabo quam
           ducimus esse non adipisci.
         </p>
-        <div className="feature-options">
-          <button className="feature-options--play">
+        <div className="feature_options">
+          <button className="feature_options--play">
             <PlayCircleFilledRounded />
             Play
           </button>
-          <button className="feature-options--more">
+          <button className="feature_options--more">
             <InfoRounded />
             Info
           </button>
