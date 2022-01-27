06c2187f409013e6410c98142bee0568672f8163
Amandine Arias <amandine.arias@gmail.com>
2022-01-27 17:56:19 +0100

    feat (Slider) : dynamic items

--- src/components/slider/index.jsx
+++ src/components/slider/index.jsx
@@ -1,20 +1,31 @@
-import { useState, useRef } from 'react';
-// import { slides } from './data';
+import { useState, useEffect, useRef } from 'react';
+import { Link } from "react-router-dom"
+import axios from 'axios';
 
 import {
   ArrowBackIosNewOutlined,
   ArrowForwardIosOutlined,
 } from '@mui/icons-material';
 
-import Item from '../Item';
-
 import './slider.css';
 
-export default function Slider() {
+export default function Slider({title, fetchUrl, isPoster}) {
+
+  const BASE_URL = "https://image.tmdb.org/t/p/original"
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl)
+      setMovies(request.data.results)
+    }
+    fetchData()
+  }, [fetchUrl])
+
+  console.log(movies)
+
   const [slideNumber, setSlideNumber] = useState(0);
-
   const sliderRef = useRef();
-
   const handleClick = (slide) => {
     let distance = sliderRef.current.getBoundingClientRect().x - 28;
 
@@ -31,7 +42,7 @@ export default function Slider() {
 
   return (
     <div className="slider">
-      <h2 className="slider_title">Continue to watch</h2>
+      <h2 className="slider_title">{title}</h2>
       <div className="slider_wrapper">
           <button
           className="slider_arrow slider_arrow--left"
@@ -40,16 +51,25 @@ export default function Slider() {
           <ArrowBackIosNewOutlined />
         </button>
         <div className="slider_content" ref={sliderRef}>
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
+        {movies.map(movie => (
+          <div className="item" key={movie.id}>
+            <Link to={`/video/${movie.id}`}>
+              {isPoster ? (
+                <img
+                  src={`${BASE_URL}/${movie.poster_path}`}
+                  className="row__image"
+                  alt={movie?.title || movie?.name || movie?.original_title}
+                />
+              ) : (
+                <img
+                  src={`${BASE_URL}/${movie.backdrop_path}`}
+                  className="row__image"
+                  alt={movie?.title || movie?.name || movie?.original_title}
+                />
+              )}
+            </Link>
+          </div>
+        ))}
         </div>
         <button
           className="slider_arrow slider_arrow--right"
