057209ca40cbf259267f1e02cd48553223c7d8f0
Amandine Arias <amandine.arias@gmail.com>
2022-02-03 19:21:58 +0100

    update (Slider) : improve

--- src/components/slider/index.jsx
+++ src/components/slider/index.jsx
@@ -7,6 +7,8 @@ import {
   ArrowForwardIosOutlined,
 } from '@mui/icons-material';
 
+import Popup from '../Popup';
+
 import './slider.css';
 
 export default function Slider({ title, fetchUrl, isPoster }) {
@@ -30,44 +32,71 @@ export default function Slider({ title, fetchUrl, isPoster }) {
 
     if (slide === 'previous' && slideNumber > 0) {
       setSlideNumber(slideNumber - 1);
-      sliderRef.current.style.transform = `translateX(${230 + distance}px)`;
+      sliderRef.current.style.transform = `translateX(${460 + distance}px)`;
     }
 
-    if (slide === 'next' && slideNumber < 4) {
+    if (slide === 'next' && slideNumber < 7) {
       setSlideNumber(slideNumber + 1);
-      sliderRef.current.style.transform = `translateX(${-230 + distance}px)`;
+      sliderRef.current.style.transform = `translateX(${-460 + distance}px)`;
     }
   };
+
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
     <div className="slider">
       <h2 className="slider_title">{title}</h2>
       <div className="slider_wrapper">
         <button
-          className="slider_arrow slider_arrow--left"
+          className="slider_wrapper--nav slider_wrapper--navLeft"
           onClick={() => handleClick('previous')}
         >
           <ArrowBackIosNewOutlined />
         </button>
-        <div className="slider_content" ref={sliderRef}>
+        <div className="slider_wrapper--content" ref={sliderRef}>
           {movies.map((movie) => (
-            <div className="item" key={movie.id}>
-              <Link to={`/video/${movie.id}`}>
-                <img
-                  src={
-                    isPoster
-                      ? `${IMG_URL}/${movie.poster_path}`
-                      : `${IMG_URL}/${movie.backdrop_path}`
-                  }
-                  className="slider_image"
-                  alt={movie?.title || movie?.name || movie?.original_title}
+            <div
+              className="slider_wrapper--item"
+              key={movie.id}
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+            >
+              {isHovered ? (
+                <Popup
+                  key={`popup ${movie.id}`}
+                  vote={movie.vote_average * 10}
+                  date={movie?.release_date || movie?.first_air_date}
+                  genre={movie.genre_ids.join(', ')}
+                  overview={movie.overview}
                 />
-              </Link>
+              ) : (
+                <Link
+                  to={`/video/${movie.id}`}
+                  key={`poster ${movie.id}`}>
+                  <img
+                    // src={
+                    //   isPoster
+                    //     ? `${IMG_URL}/${movie.poster_path}`
+                    //     : `${IMG_URL}/${movie.backdrop_path}`
+                    // }
+                    src={`${IMG_URL}/${movie.poster_path}`}
+                    className="slider_wrapper--image"
+                    alt={movie?.title || movie?.name || movie?.original_title}
+                  />
+                </Link>
+              )}
+
+              {/* {isPoster ? null : (
+                    <h3 className="slider_wrapper--title">
+                      {movie?.title || movie?.name || movie?.original_title}
+                    </h3>
+                    )} */}
             </div>
           ))}
         </div>
+        {/* )} */}
         <button
-          className="slider_arrow slider_arrow--right"
+          className="slider_wrapper--nav slider_wrapper--navRight"
           onClick={() => handleClick('next')}
         >
           <ArrowForwardIosOutlined />
@@ -76,3 +105,100 @@ export default function Slider({ title, fetchUrl, isPoster }) {
     </div>
   );
 }
+// import { useState, useEffect, useRef } from 'react';
+// import { Link } from 'react-router-dom';
+// import axios from 'axios';
+
+// import {
+//   ArrowBackIosNewOutlined,
+//   ArrowForwardIosOutlined,
+// } from '@mui/icons-material';
+
+// import Popup from '../Popup';
+
+// import './slider.css';
+
+// export default function Slider({ title, fetchUrl, isPoster }) {
+//   const IMG_URL = 'https://image.tmdb.org/t/p/original';
+//   const [movies, setMovies] = useState([]);
+
+//   useEffect(() => {
+//     async function fetchData() {
+//       const request = await axios.get(fetchUrl);
+//       setMovies(request.data.results);
+//     }
+//     fetchData();
+//   }, [fetchUrl]);
+
+//   console.log(movies);
+
+//   const [slideNumber, setSlideNumber] = useState(0);
+//   const sliderRef = useRef();
+//   const handleClick = (slide) => {
+//     let distance = sliderRef.current.getBoundingClientRect().x - 28;
+
+//     if (slide === 'previous' && slideNumber > 0) {
+//       setSlideNumber(slideNumber - 1);
+//       sliderRef.current.style.transform = `translateX(${460 + distance}px)`;
+//     }
+
+//     if (slide === 'next' && slideNumber < 7) {
+//       setSlideNumber(slideNumber + 1);
+//       sliderRef.current.style.transform = `translateX(${-460 + distance}px)`;
+//     }
+//   };
+
+//   const [isHovered, setIsHovered] = useState(false);
+
+//   return (
+//     <div className="slider">
+//       <h2 className="slider_title">{title}</h2>
+//       <div
+//         className="slider_wrapper"
+//         // style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+//         onMouseEnter={() => setIsHovered(true)}
+//         onMouseLeave={() => setIsHovered(false)}
+//       >
+//         <button
+//           className="slider_wrapper--nav slider_wrapper--navLeft"
+//           onClick={() => handleClick('previous')}
+//         >
+//           <ArrowBackIosNewOutlined />
+//         </button>
+//         {isHovered ? (
+//           <Popup />
+//         ) : (
+//           <div className="slider_wrapper--content" ref={sliderRef}>
+//             {movies.map((movie) => (
+//               <div className="slider_wrapper--item" key={movie.id}>
+//                 <Link to={`/video/${movie.id}`}>
+//                   <img
+//                     // src={
+//                     //   isPoster
+//                     //     ? `${IMG_URL}/${movie.poster_path}`
+//                     //     : `${IMG_URL}/${movie.backdrop_path}`
+//                     // }
+//                     src={`${IMG_URL}/${movie.poster_path}`}
+//                     className="slider_wrapper--image"
+//                     alt={movie?.title || movie?.name || movie?.original_title}
+//                   />
+//                 </Link>
+//                 {/* {isPoster ? null : (
+//                     <h3 className="slider_wrapper--title">
+//                       {movie?.title || movie?.name || movie?.original_title}
+//                     </h3>
+//                     )} */}
+//               </div>
+//             ))}
+//           </div>
+//         )}
+//         <button
+//           className="slider_wrapper--nav slider_wrapper--navRight"
+//           onClick={() => handleClick('next')}
+//         >
+//           <ArrowForwardIosOutlined />
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
