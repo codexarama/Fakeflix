8da73a9f703e3f92edfc484410c7e9f69a5bbe69
Amandine Arias <amandine.arias@gmail.com>
2022-02-03 19:16:11 +0100

    feat (requests) : Netflix Originals

--- src/config/requests.js
+++ src/config/requests.js
@@ -5,6 +5,7 @@ const requests = {
   // fetchBest: `${BASE_URL}/discover/movie?api_key=${API_KEY}&primary_release_year=2021&sort_by=vote_average.desc`,
   // fetchPopular: `${BASE_URL}/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc`,
   fetchTrending: `${BASE_URL}/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `${BASE_URL}/discover/tv?api_key=${API_KEY}&language=en-US&with_network=213`,
   fetchTopRatedMovie: `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchTopRatedTvShow: `${BASE_URL}/tv/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchSciFiMovies: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=878`,
