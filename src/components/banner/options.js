0f0ee4f784302ec723d47b63c84dd43548df8e49
Amandine Arias <amandine.arias@gmail.com>
2022-02-06 20:22:31 +0100

    feat (Banner) : add options for select genres component

--- src/components/banner/options.js
+++ src/components/banner/options.js
@@ -5,12 +5,15 @@ export const options = [
   { value: 'crime', label: 'Crime' },
   { value: 'drama', label: 'Drama' },
   { value: 'documentary', label: 'Documentary' },
+  { value: 'family', label: 'Family' },
   { value: 'fantasy', label: 'Fantasy' },
   { value: 'historical', label: 'Historical' },
   { value: 'horror', label: 'Horror' },
+  { value: 'mystery', label: 'Mystery' },
   { value: 'romance', label: 'Romance' },
   { value: 'sci-fi', label: 'Sci-fi' },
   { value: 'thriller', label: 'Thriller' },
+  { value: 'war', label: 'War' },
   { value: 'western', label: 'Western' },
 ];
 
