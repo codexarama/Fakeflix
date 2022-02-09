53d4444fbf5ea5c79489f81fa947dae6cee2986b
Amandine Arias <amandine.arias@gmail.com>
2022-02-09 20:13:00 +0100

    feat (Component) : Error

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Home from './Home';
 import { Video } from './components/Features';
 import Footer from './components/Footer';
+import Error from './Error';
 
 import './app.css';
 
@@ -20,6 +21,7 @@ export default function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/video/:id" element={<Video />} />
+        <Route path="*" element={<Error />} />
       </Routes>
 
       <Footer />
