f702ff0738572abb1d25bfbc43f6cec2e2947778
Amandine Arias <amandine.arias@gmail.com>
2022-02-09 13:52:34 +0100

    refactor (Teaser)

--- src/components/Teaser/index.jsx
+++ src/components/Teaser/index.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { IMG_URL } from '../../config/requests';
 
-import { Content, Header, Icons } from '../../Features';
+import { Content, Header, Icons } from '../Features';
 
 import './teaser.css';
-import '../../Features/features.css';
+import '../Features/features.css';
 
 export default function Teaser({
-  image,
+  banner,
   index,
   id,
   poster,
@@ -17,21 +18,21 @@ export default function Teaser({
   vote,
   synopsis,
 }) {
-  const IMG_URL = 'https://image.tmdb.org/t/p/original';
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <>
+    <section
+      className="slider_wrapper--item"
+      style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       {isHovered ? (
-        <section
-          className="slider_teaser"
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-        >
+        <>
           <Header
             className="slider_teaser--header"
             style={{
-              backgroundImage: `url("https://image.tmdb.org/t/p/original/${image?.backdrop_path}")`,
+              backgroundImage: `url(${IMG_URL}${banner})`,
               backgroundSize: 'cover',
               backgroundPosition: 'center center',
             }}
@@ -45,23 +46,16 @@ export default function Teaser({
               synopsis={synopsis}
             />
           </main>
-        </section>
+        </>
       ) : (
-        <section
-          className="slider_wrapper--item"
-          style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-        >
-          <Link to={`/video/${id}`} key={`poster ${id}`}>
-            <img
-              src={`${IMG_URL}/${poster}`}
-              className="slider_wrapper--image"
-              alt={title}
-            />
-          </Link>
-        </section>
+        <Link to={`/video/${id}`} key={`poster ${id}`}>
+          <img
+            src={`${IMG_URL}${poster}`}
+            className="slider_wrapper--image"
+            alt={title}
+          />
+        </Link>
       )}
-    </>
+    </section>
   );
 }
