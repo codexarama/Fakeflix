459eb39465a612fa9dc365c2f070e5911408c127
Amandine Arias <amandine.arias@gmail.com>
2022-02-09 13:47:16 +0100

    feat (request) : url variable to use anywhere else

--- src/config/requests.js
+++ src/config/requests.js
@@ -1,5 +1,6 @@
 export const API_KEY = '7cd87a2a8f9768f5fe4154575d2b60bb';
 export const BASE_URL = 'https://api.themoviedb.org/3';
+export const IMG_URL = 'https://image.tmdb.org/t/p/original/';
 
 const requests = {
   // fetchBest: `${BASE_URL}/discover/movie?api_key=${API_KEY}&primary_release_year=2021&sort_by=vote_average.desc`,
