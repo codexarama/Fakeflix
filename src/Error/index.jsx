12445d12e85654fab686365b3dce42d7051c9f34
Amandine Arias <amandine.arias@gmail.com>
2022-02-10 21:27:50 +0100

    feat (Error) : 404 animation

--- src/Error/index.jsx
+++ src/Error/index.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import { useEffect } from 'react';
+
+import error from '../assets/404.gif';
+import './error.css'
 
 export default function Error() {
+  useEffect(() => {
+    document
+      .querySelector('footer').style.display = 'none';
+  }, []);
+
   return (
-    <div>Error</div>
-  )
+    <main className='error'>
+      <img src={error} alt="error page animation" />
+    </main>
+  );
 }
