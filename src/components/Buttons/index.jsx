b3caab190dcb412e1e26899a46f307d9e987f152
Amandine Arias <amandine.arias@gmail.com>
2022-03-23 14:01:20 +0100

    Style (Action) : custom & dynamic add button

--- src/components/Buttons/index.jsx
+++ src/components/Buttons/index.jsx
@@ -4,16 +4,19 @@ import { GlobalContext } from '../../context/GlobalState';
 import { Link } from 'react-router-dom';
 
 import {
+  Check,
   Add,
   PlayCircleFilled,
   ThumbUpOffAlt,
   ThumbDownOffAlt,
 } from '@mui/icons-material';
 
-import './buttons.css'
+import './buttons.css';
 
 export default function Icons({ className, add, videoLink }) {
-  const {addMovieToWatchList} = useContext(GlobalContext)
+  const { addMovieToWatchList, watchList } = useContext(GlobalContext);
+  let storedMovie = watchList.find((item) => item.id === add.id);
+  const addDisabled = storedMovie ? true : false;
 
   return (
     <section className={`group_icons ${className}`}>
@@ -22,9 +25,21 @@ export default function Icons({ className, add, videoLink }) {
           <PlayCircleFilled />
         </button>
       </Link>
-      <button className="icon icon_add icon_yes" onClick={() => addMovieToWatchList(add)}>
-        <Add />
-      </button>
+      {addDisabled ? (
+        <button
+          className="icon icon_check"
+          disabled={addDisabled}
+        >
+          <Check />
+        </button>
+      ) : (
+        <button
+          className="icon icon_add icon_yes"
+          onClick={() => addMovieToWatchList(add)}
+        >
+          <Add />
+        </button>
+      )}
       <button className="icon icon_thumb icon_yes">
         <ThumbUpOffAlt />
       </button>
