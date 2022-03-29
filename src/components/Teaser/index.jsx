ca9cb84fd3af2ae13336b94cc9e0dc5e2ab78b7e
Amandine Arias <amandine.arias@gmail.com>
2022-03-29 17:41:20 +0200

    refactor (Teaser) : simplify
    more efficient & reusable with less code

--- src/components/Teaser/index.jsx
+++ src/components/Teaser/index.jsx
@@ -4,20 +4,11 @@ import { IMG_URL } from '../../config/requests';
 
 import Icons from '../Buttons';
 import Content from '../Content';
+import { genreFinder } from '../Content/genres';
 
 import './teaser.css';
 
-export default function Teaser({
-  vignette,
-  addMovie,
-  movieId,
-  title,
-  date,
-  genre,
-  vote,
-  synopsis,
-  poster,
-}) {
+export default function Teaser({ movie }) {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
@@ -31,7 +22,7 @@ export default function Teaser({
           <header
             className="teaser_header"
             style={{
-              backgroundImage: `url(${IMG_URL}${vignette})`,
+              backgroundImage: `url(${IMG_URL}${movie.backdrop_path})`,
               backgroundSize: 'cover',
               backgroundPosition: 'center center',
             }}
@@ -39,19 +30,28 @@ export default function Teaser({
             <div className="teaser_vignette"></div>
           </header>
           <main className="teaser_infos">
-            <Icons addMovie={addMovie} movieId={movieId} />
+            <Icons addMovie={movie} movieId={movie.id} />
             <Content
-              genre={genre}
+              genre={genreFinder(movie)}
               className="specific_style"
-              vote={vote}
-              date={date}
-              synopsis={synopsis}
+              vote={movie.vote_average * 10}
+              // get only yyyy from format date yyyy-mm-dd
+              // THE SECRET : add "?" after key "realease_date" || "first_air_date"
+              date={
+                movie.release_date?.slice(0, -6) ||
+                movie.first_air_date?.slice(0, -6)
+              }
+              synopsis={movie?.overview || 'No description available'}
             />
           </main>
         </>
       ) : (
-        <Link to={`/video/${movieId}`} key={`poster ${movieId}`}>
-          <img src={`${IMG_URL}${poster}`} className="item_image" alt={title} />
+        <Link to={`/video/${movie.id}`} key={`poster ${movie.id}`}>
+          <img
+            src={`${IMG_URL}${movie.poster_path}`}
+            className="item_image"
+            alt={movie?.title || movie?.name || movie?.original_title}
+          />
         </Link>
       )}
     </li>
