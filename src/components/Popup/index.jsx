71c0428b7e3218d014f1a64634f04b9cee7f644b
Amandine Arias <amandine.arias@gmail.com>
2022-03-30 10:18:53 +0200

    refactor (Popup) : improve code

--- src/components/Popup/index.jsx
+++ src/components/Popup/index.jsx
@@ -1,28 +1,22 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
-import { IMG_URL } from '../../config/requests';
+import axios from 'axios';
+import {
+  // MOVIE_ID,
+  CREDIT_URL_START,
+  CREDIT_URL_END,
+} from '../../config/requests';
 
 import { Cancel } from '@mui/icons-material';
-
+import Header from '../Header';
 import Icons from '../Buttons';
 import Content from '../Content';
+import { genreFinder } from '../Content/genres';
 
 import './popup.css';
 
-export default function Popup({
-  popup,
-  close,
-  vignette,
-  addMovie,
-  movieId,
-  title,
-  vote,
-  synopsis,
-  date,
-  casting,
-  genre,
-}) {
+export default function Popup({ popup, close, movie }) {
   // handle ARIA attributes
   // prevent body from scrolling when popup is open
   useEffect(() => {
@@ -35,6 +29,27 @@ export default function Popup({
     !popup && document.body.setAttribute('arias-hidden', 'false');
     !popup && (document.body.style.overflow = 'unset');
   }, [popup]);
+
+  // get casting data
+  const credits_URL = CREDIT_URL_START + `${movie?.id}` + CREDIT_URL_END;
+  // console.log(MOVIE_ID);
+  // console.log(MOVIE_ID = "ola"); // Cannot set property MOVIE_ID of #<Object> which has only a getter
+  // MOVIE_ID = `${movie?.id}`
+  // const credits_URL = requests.credits;
+
+  const [casting, setCasting] = useState([]);
+
+  useEffect(() => {
+    async function fetchCasting() {
+      const credits = await axios.get(credits_URL);
+      
+      // get the 3 main actors
+      if (credits.data.cast.length > 3) credits.data.cast.length = 3;
+      setCasting(credits.data.cast);
+    }
+
+    fetchCasting();
+  }, [credits_URL]);
 
   return createPortal(
     <>
@@ -52,27 +67,33 @@ export default function Popup({
             <button className="popup_close" onClick={close}>
               <Cancel className="popup_close--icon" />
             </button>
-            <header
-              className="popup_header"
-              style={{
-                backgroundImage: `url(${IMG_URL}${vignette})`,
-                backgroundSize: 'cover',
-                backgroundPosition: 'center center',
-              }}
-            >
-              <div className="teaser_vignette"></div>
-            </header>
+            <Header className="popup_header" movie={movie} />
             <Icons
               className={'popup_icons'}
-              addMovie={addMovie}
-              movieId={movieId}
+              addMovie={movie}
+              movieId={movie.id}
             />
             <section className="popup_main">
               <article className="popup_content">
-                <Content title={title} vote={vote} synopsis={synopsis} />
+                <Content
+                  title={movie?.title || movie?.name || movie?.original_title}
+                  vote={movie.vote_average * 10}
+                  synopsis={movie?.overview || 'No description available'}
+                />
               </article>
               <aside className="popup_aside">
-                <Content date={date} casting={casting} genre={genre} />
+                <Content
+                  date={
+                    movie.release_date?.slice(0, -6) ||
+                    movie.first_air_date?.slice(0, -6)
+                  }
+                  casting={casting.map((actor) => (
+                    <li key={actor?.name} className="teaser_infos--casting">
+                      {actor?.name}
+                    </li>
+                  ))}
+                  genre={genreFinder(movie)}
+                />
               </aside>
             </section>
           </section>
