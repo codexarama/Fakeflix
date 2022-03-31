0393ae32d8734469a9435823d92eea16e406269c
Amandine Arias <amandine.arias@gmail.com>
2022-03-31 12:42:37 +0200

    feat (usePopup) : redirect after closing
    choose redirection url when close component

--- src/components/Popup/usePopup.js
+++ src/components/Popup/usePopup.js
@@ -1,13 +1,17 @@
 // CUSTOM POPUP HOOK
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function usePopup() {
   // popup's state
   const [isOpen, setIsOpen] = useState(false);
 
   // close popup when open
-  function toggle() {
+  const navigate = useNavigate()
+  function toggle(url) {
     setIsOpen(!isOpen);
+    navigate( `/${url}`, { replace: true });
+  // console.log(url);
   }
 
   // close popup by pressing escape key when keyboard navigation
@@ -17,16 +21,11 @@ export default function usePopup() {
     }
   }
 
+  // remove "escapeToClose" event from DOM once occurred
   function keyboardEscape() {
     window.addEventListener('keydown', escapeToClose);
     return () => window.removeEventListener('keydown', escapeToClose);
   }
-
-  // function escToClose(e) {
-  //   if (e.key === 'Escape') {
-  //     setIsOpen(false)
-  //   }
-  // }
 
   return {
     isOpen,
