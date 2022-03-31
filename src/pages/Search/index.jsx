f8c79c2c1830628a02c2c15bf4ad2cf28ba8fbe8
Amandine Arias <amandine.arias@gmail.com>
2022-03-31 12:53:15 +0200

    refactor (Search) : re-use SearchMovie component

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,53 +1,9 @@
-import { useState } from 'react';
-
-import axios from 'axios';
-import requests from '../../config/requests';
-
-import Teaser from '../../components/Teaser';
-
-import { Search } from '@mui/icons-material';
-
-import './search.css';
+import SearchMovie from '../../components/Search';
 
 export default function SearchPage() {
-  const [query, setQuery] = useState('');
-  const [results, setResults] = useState([]);
-
-  function onChange(event) {
-    event.preventDefault();
-    setQuery(event.target.value);
-
-    // console.log(requests.search);
-    const search_URL = `${requests.search}${event.target.value}`;
-
-    async function fetchData() {
-      const request = await axios.get(search_URL);
-      setResults(request.data.results);
-    }
-
-    fetchData();
-  }
-
-  //   console.log(results);
-
   return (
     <main className="main_content">
-      <form action="" className="search_movie">
-          <input
-            type="text"
-            placeholder="Search for a movie"
-            autoComplete="off"
-            value={query}
-            onChange={onChange}
-          />
-        <Search className="icon" />
-      </form>
-      <ul className="main_content--results">
-        {results.length > 0 &&
-          results.map((movie) => (
-            <Teaser key={movie.id} movie={movie} />
-          ))}
-      </ul>
+      <SearchMovie />
     </main>
   );
 }
