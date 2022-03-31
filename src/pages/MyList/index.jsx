45df191c44e59dbec32e83a4be8ce98d5f54ae41
Amandine Arias <amandine.arias@gmail.com>
2022-03-31 12:54:16 +0200

    refactor (MyList) : re-use SearchMovie component

--- src/pages/MyList/index.jsx
+++ src/pages/MyList/index.jsx
@@ -1,9 +1,11 @@
 import { useContext } from 'react';
 import { GlobalContext } from '../../context/GlobalState';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 import Teaser from '../../components/Teaser';
-import { Search } from '@mui/icons-material';
+
+import '../../components/Search/search.css';
+import SearchMovie from '../../components/Search';
 
 export default function MyList() {
   const { watchList } = useContext(GlobalContext);
@@ -20,12 +22,7 @@ export default function MyList() {
       ) : (
         <>
           <p className="main_content--info">No movies in your list yet</p>
-          <Link to={'/search'}>
-            <button className="main_content--search">
-              <Search className="icon" />
-              Find movies
-            </button>
-          </Link>
+          <SearchMovie />
         </>
       )}
     </main>
