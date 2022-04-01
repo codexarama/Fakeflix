ad1d8a89f9cc232b6a6689ae4bd8bda639a7e823
Amandine Arias <amandine.arias@gmail.com>
2022-04-01 12:11:32 +0200

    feat (Search) : JsDoc & PropTypes

--- src/components/Search/Input.jsx
+++ src/components/Search/Input.jsx
@@ -1,5 +1,17 @@
+import PropTypes from 'prop-types';
+
 import { Search } from '@mui/icons-material';
 
+/**
+ * Input COMPONENT
+ *
+ * @param   {object}      props
+ * @param   {string}      props.query      [any character]
+ * @param   {function}    props.onChange   [handle input change]
+ * @param   {function}    props.onSubmit   [handle form submit]
+ *
+ * @returns {Reactnode}   jsx in DOM
+ */
 export default function Input({ query, onChange, onSubmit }) {
   return (
     <form className="modal_search--form" action="" autoComplete="on">
@@ -16,3 +28,12 @@ export default function Input({ query, onChange, onSubmit }) {
     </form>
   );
 }
+
+/**
+ * Input PROPTYPES
+ */
+ Input.propTypes = {
+  query: PropTypes.string,
+  onChange: PropTypes.func,
+  onSubmit: PropTypes.func,
+};
