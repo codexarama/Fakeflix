8e361717957ac73cca66e10d03f581f3fef01bca
Amandine Arias <amandine.arias@gmail.com>
2022-04-01 12:26:37 +0200

    feat (Search) : JsDoc

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,5 +1,9 @@
 import SearchMovie from '../../components/Search';
 
+/**
+ * SearchPage COMPONENT
+ * @returns {Reactnode}   jsx in DOM
+ */
 export default function SearchPage() {
   return (
     <main className="main_content">
