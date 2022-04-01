27bd23437215df905396fb6cfa8b34078221a6f7
Amandine Arias <amandine.arias@gmail.com>
2022-04-01 20:29:14 +0200

    refactor (Search) : simplify

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,21 +1,33 @@
-import { useEffect, useState } from 'react';
-
+import { useEffect, useState, useRef } from 'react';
 import requests from '../../config/requests';
 import axios from 'axios';
+import Teaser from '../../components/Teaser';
 
-import usePopup from '../Popup/usePopup';
-import Input from './Input';
-import Results from './Results';
+import { Search } from '@mui/icons-material';
 
 import './search.css';
-import '../Popup/popup.css';
 
+/**
+ * SearchPage COMPONENT
+ * @returns {Reactnode}   jsx in DOM
+ */
 export default function SearchMovie() {
-  const [query, setQuery] = useState('');
+  const [inputValue, setInputValue] = useState('');
+
+  // useRef Hook allows to persist values between renders
+  // and stores a mutable value without causing a re-render when updated.
+  // by accessing a DOM element directly.
+  const previousInputValue = useRef('');
+
+  useEffect(() => {
+    previousInputValue.current = inputValue;
+  }, [inputValue]);
+
+  const [results, setResults] = useState([]);
+
   function onChange(event) {
     event.preventDefault();
-    setQuery(event.target.value);
-
+    setInputValue(event.target.value);
     // console.log(requests.search);
     const search_URL = `${requests.search}${event.target.value}`;
 
@@ -27,36 +39,31 @@ export default function SearchMovie() {
     fetchData();
   }
 
-  const [results, setResults] = useState([]);
   // console.log(results);
-  function onSubmit() {
-    const searchPage = 'search';
-    results.length > 0 && toggle(searchPage);
-  }
-
-  // handle Popup || Modal component actions
-  const { isOpen, toggle, keyboardEscape } = usePopup();
-  // press escape to close Popup || Modal component
-  useEffect(() => {
-    keyboardEscape();
-  });
-
-  const mylistPage = 'mylist';
 
   return (
-    <>
-      <Input query={query} onChange={onChange} onSubmit={onSubmit} />
-
-      {/* DISPLAYS SEARCH RESULTS IN A MODAL
-          AND THE SEARCH INPUT WITH FIRST ENTRIE
-          WHICH CAN BE COMPLETED || UPDATED  */}
-      <Results
-        popup={isOpen}
-        close={() => toggle(mylistPage)}
-        query={query}
-        onChange={onChange}
-        results={results}
-      />
-    </>
+    <main className="main_content">
+      <form className="search_form" action="" autoComplete="on">
+        <label />
+        <input
+          className="search_input"
+          type="text"
+          placeholder="Search for a movie"
+          autoFocus
+          value={inputValue}
+          onChange={onChange}
+        />
+        <Search className="icon search_submit" />
+      </form>
+      {inputValue ? (
+        <>
+          <h2 className="search_msg">{`${results.length} résultats`}</h2>
+          <ul className="main_content--results">
+            {results.length > 0 &&
+              results.map((movie) => <Teaser key={movie.id} movie={movie} />)}
+          </ul>
+        </>
+      ) : null}
+    </main>
   );
 }
