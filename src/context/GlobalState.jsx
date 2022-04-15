2fb094d733672710babf8a53848f8814367d8bf7
Amandine Arias <amandine.arias@gmail.com>
2022-04-15 12:34:07 +0200

    feat (context) : store movie rating values

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -6,37 +6,56 @@ const initialState = {
   watchList: localStorage.getItem('watchList')
     ? JSON.parse(localStorage.getItem('watchList'))
     : [],
+
+  rating: localStorage.getItem('rating')
+    ? JSON.parse(localStorage.getItem('rating'))
+    : [],
 };
 
 // create context
 export const GlobalContext = createContext(initialState);
 
 // provider components
-export default function GlobalProvider(props) {
+export default function GlobalProvider({ children }) {
   const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  const { watchList, rating } = state;
 
   useEffect(() => {
     localStorage.setItem('watchList', JSON.stringify(state.watchList));
+    localStorage.setItem('rating', JSON.stringify(state.rating));
   }, [state]);
 
-  // actions
-  const addMovieToWatchList = (movie) => {
+  // ACTIONS
+  function addMovieToWatchList(movie) {
     dispatch({ type: 'ADD_MOVIE_TO_WATCHLIST', payload: movie });
-  };
+  }
 
-  const removeMovieFromWatchList = (id) => {
+  function removeMovieFromWatchList (id) {
     dispatch({ type: 'REMOVE_MOVIE_FROM_WATCHLIST', payload: id });
   };
 
+  function getVoteCount (movie) {
+    dispatch({
+      type: 'GET_VOTE_COUNT',
+      payload: {
+        id: movie.id,
+        name: movie?.title || movie?.name || movie?.original_title,
+        vote_count: movie.vote_count,
+      },
+    });
+  };
+
+  // VALUES
+  const value = {
+    watchList,
+    addMovieToWatchList,
+    removeMovieFromWatchList,
+    rating,
+    getVoteCount,
+  };
+
   return (
-    <GlobalContext.Provider
-      value={{
-        watchList: state.watchList,
-        addMovieToWatchList,
-        removeMovieFromWatchList,
-      }}
-    >
-      {props.children}
-    </GlobalContext.Provider>
+    <GlobalContext.Provider value={value}>{children}</GlobalContext.Provider>
   );
 }
