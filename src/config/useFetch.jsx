a319e9f7341a3eb94e855404824661dddfb47cac
Amandine Arias <amandine.arias@gmail.com>
2022-06-10 13:03:15 +0200

    refactor (useFetch) : axios params

--- src/config/useFetch.jsx
+++ src/config/useFetch.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useReducer } from 'react';
+import { REACT_APP_API_KEY } from './requests';
 import axios from 'axios';
 
 /**
@@ -7,56 +8,60 @@ import axios from 'axios';
  * @returns {string}   	  state		[loading || fetched || error]
  */
 export const useFetch = (url) => {
-	const cache = useRef({});
+  const cache = useRef({});
 
-	const initialState = {
-		status: '',
-		error: null,
-		data: [],
-	};
+  const initialState = {
+    status: '',
+    error: null,
+    data: [],
+  };
 
-	const [state, dispatch] = useReducer((state, action) => {
-		switch (action.type) {
-			case 'LOADING':
-				return { ...initialState, status: 'loading' };
-			case 'FETCHED':
-				return { ...initialState, status: 'fetched', data: action.payload };
-			case 'ERROR':
-				return { ...initialState, status: 'error', error: action.payload };
-			default:
-				return state;
-		}
-	}, initialState);
+  const [state, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case 'LOADING':
+        return { ...initialState, status: 'loading' };
+      case 'FETCHED':
+        return { ...initialState, status: 'fetched', data: action.payload };
+      case 'ERROR':
+        return { ...initialState, status: 'error', error: action.payload };
+      default:
+        return state;
+    }
+  }, initialState);
 
-	useEffect(() => {
-		let cancelRequest = false;
-		if (!url || !url.trim()) return;
+  useEffect(() => {
+    let cancelRequest = false;
+    if (!url || !url.trim()) return;
 
-		const fetchData = async () => {
-			dispatch({ type: 'LOADING' });
-			if (cache.current[url]) {
-				const data = cache.current[url];
-				dispatch({ type: 'FETCHED', payload: data });
-			} else {
-				try {
-					const request = await axios.get(url);
-					const data = request.data.results;
-					cache.current[url] = data;
-					if (cancelRequest) return;
-					dispatch({ type: 'FETCHED', payload: data });
-				} catch (error) {
-					if (cancelRequest) return;
-					dispatch({ type: 'ERROR', payload: error.message });
-				}
-			}
-		};
+    const fetchData = async () => {
+      dispatch({ type: 'LOADING' });
+      if (cache.current[url]) {
+        const data = cache.current[url];
+        dispatch({ type: 'FETCHED', payload: data });
+      } else {
+        try {
+          const request = await axios.get(url, {
+            params: {
+              api_key: REACT_APP_API_KEY,
+            },
+          });
+          const data = request.data.results;
+          cache.current[url] = data;
+          if (cancelRequest) return;
+          dispatch({ type: 'FETCHED', payload: data });
+        } catch (error) {
+          if (cancelRequest) return;
+          dispatch({ type: 'ERROR', payload: error.message });
+        }
+      }
+    };
 
-		fetchData();
+    fetchData();
 
-		return function cleanup() {
-			cancelRequest = true;
-		};
-	}, [url]);
+    return function cleanup() {
+      cancelRequest = true;
+    };
+  }, [url]);
 
-	return state;
+  return state;
 };
