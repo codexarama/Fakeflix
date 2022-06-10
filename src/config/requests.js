c2b670b414d5d490005aeeaf784529cfa4b368ba
Amandine Arias <amandine.arias@gmail.com>
2022-06-10 13:05:16 +0200

    update (requests) : remove API_KEY from url
    managed by useFetch as axios parameter

--- src/config/requests.js
+++ src/config/requests.js
@@ -1,26 +1,24 @@
-// export const {REACT_APP_API_KEY} = process.env;
-const REACT_APP_API_KEY="7cd87a2a8f9768f5fe4154575d2b60bb"
-export const BASE_URL = 'https://api.themoviedb.org/3';
+const BASE_URL = 'https://api.themoviedb.org/3';
 export const IMG_URL = 'https://image.tmdb.org/t/p/original/';
-export const CREDIT_URL_START = `${BASE_URL}/movie/`
-export const CREDIT_URL_END = `/credits?api_key=${REACT_APP_API_KEY}&language=en-US`
 
-// export let {MOVIE_ID} = ""
+// * use this way in real context
+// export const {REACT_APP_API_KEY} = process.env;
+// ! needs to be visible to get data via Netlify web editor
+export const REACT_APP_API_KEY="7cd87a2a8f9768f5fe4154575d2b60bb"
 
 const requests = {
-  // fetchBest: `${BASE_URL}/discover/movie?api_key=${REACT_APP_API_KEY}&primary_release_year=2021&sort_by=vote_average.desc`,
-  // fetchPopular: `${BASE_URL}/discover/movie?api_key=${REACT_APP_API_KEY}&sort_by=popularity.desc`,
-  fetchTrending: `${BASE_URL}/trending/all/week?api_key=${REACT_APP_API_KEY}&language=en-US`,
-  fetchNetflixOriginals: `${BASE_URL}/discover/tv?api_key=${REACT_APP_API_KEY}&language=en-US&with_network=213`,
-  fetchTopRatedMovie: `${BASE_URL}/movie/top_rated?api_key=${REACT_APP_API_KEY}&language=en-US`,
-  fetchTopRatedTvShow: `${BASE_URL}/tv/top_rated?api_key=${REACT_APP_API_KEY}&language=en-US`,
-  fetchSciFiMovies: `${BASE_URL}/discover/movie?api_key=${REACT_APP_API_KEY}&language=en-US&with_genres=878`,
-  fetchAnimationMovies: `${BASE_URL}/discover/movie?api_key=${REACT_APP_API_KEY}&language=en-US&with_genres=16`,
-  fetchMysteryMovies: `${BASE_URL}/discover/movie?api_key=${REACT_APP_API_KEY}&language=en-US&with_genres=9648`,
-  fetchDramaMovies: `${BASE_URL}/discover/movie?api_key=${REACT_APP_API_KEY}&language=en-US&with_genres=18`,
-  fetchDocumentaries: `${BASE_URL}/discover/movie?api_key=${REACT_APP_API_KEY}&language=en-US&with_genres=99`,
-  // credits : `${BASE_URL}/movie/${MOVIE_ID}/credits?api_key=${REACT_APP_API_KEY}&language=en-US`,
-  search : `${BASE_URL}/search/movie?api_key=${REACT_APP_API_KEY}&language=en-US&query=`
+  // fetchBest: `${BASE_URL}/discover/movie?&primary_release_year=2021&sort_by=vote_average.desc`,
+  // fetchPopular: `${BASE_URL}/discover/movie?&sort_by=popularity.desc`,
+  fetchTrending: `${BASE_URL}/trending/all/week?&language=en-US`,
+  fetchNetflixOriginals: `${BASE_URL}/discover/tv?&language=en-US&with_network=213`,
+  fetchTopRatedMovie: `${BASE_URL}/movie/top_rated?&language=en-US`,
+  fetchTopRatedTvShow: `${BASE_URL}/tv/top_rated?&language=en-US`,
+  fetchSciFiMovies: `${BASE_URL}/discover/movie?&language=en-US&with_genres=878`,
+  fetchAnimationMovies: `${BASE_URL}/discover/movie?&language=en-US&with_genres=16`,
+  fetchMysteryMovies: `${BASE_URL}/discover/movie?&language=en-US&with_genres=9648`,
+  fetchDramaMovies: `${BASE_URL}/discover/movie?&language=en-US&with_genres=18`,
+  fetchDocumentaries: `${BASE_URL}/discover/movie?&language=en-US&with_genres=99`,
+  search : `${BASE_URL}/search/movie?&language=en-US&query=`
 };
 
 export default requests;
