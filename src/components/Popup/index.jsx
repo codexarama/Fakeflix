fd6548550a0e27031fbb70092d52ea291e2c8836
Amandine Arias <amandine.arias@gmail.com>
2022-06-12 11:45:36 +0200

    refactor (requests) : more logical namming

--- src/components/Popup/index.jsx
+++ src/components/Popup/index.jsx
@@ -4,8 +4,7 @@ import { useState, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 import axios from 'axios';
-import { useFetch } from '../../config/useFetch';
-import requests, { REACT_APP_API_KEY } from '../../config/requests';
+import requests, { API_KEY } from '../../config/requests';
 
 import { Cancel } from '@mui/icons-material';
 
@@ -41,14 +40,14 @@ export default function Popup({ popup, close, movie }) {
   }, [popup]);
 
   const [casting, setCasting] = useState([]);
-  const castingAPI = `${requests.creditsStart}${movie?.id}${requests.creditsEnd}`;
+  const castingAPI = `${requests.apiMovie}${movie?.id}${requests.credits}`;
   // console.log(castingAPI);
 
   useEffect(() => {
     async function fetchCasting() {
       const credits = await axios.get(castingAPI, {
         params: {
-          api_key: REACT_APP_API_KEY,
+          api_key: API_KEY,
         },
       });
 
